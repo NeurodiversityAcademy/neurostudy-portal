0aef1280df6bf09a0193c6b76cc724d20a08d749
saishk98 <89117911+saishk98@users.noreply.github.com>
2024-06-28 13:54:14 +0800

    Update not-found.js
    Added H3 Text

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,6 +5,7 @@ export default function NotFound() {
     <div>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
+      <h3>Test Only</h3>
       <Link href="/">Return Home</Link>
     </div>
   )
