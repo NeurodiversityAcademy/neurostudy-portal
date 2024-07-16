59a0e17f7517ef0b96813e188dd06ea7e487b9ee
saishk98 <89117911+saishk98@users.noreply.github.com>
2024-07-16 13:30:23 +0800

    Updated Contact Us Form.
    Updated Layout of the Contact Us Form.

--- src/app/components/contactUs/ContactUsForm.tsx
+++ src/app/components/contactUs/ContactUsForm.tsx
@@ -65,132 +65,140 @@ const ContactUsForm: React.FC = () => {
     <div className={styles.container}>
       <form onSubmit={(event) => handleSubmit(event)}>
         <div className={styles.title}>
-          <Typography variant={TypographyVariant.H1}>Contact Us</Typography>
-          <Typography variant={TypographyVariant.Body1}>
+          <Typography
+            variant={TypographyVariant.H3}
+            color='var(--GhostWhite)'
+            className='my-0'
+          >
+            Contact Us
+          </Typography>
+          <Typography
+            variant={TypographyVariant.Body2}
+            color='var(--GhostWhite)'
+            className='mt-0 mb-3'
+          >
             We will try our best to get back to you as soon as possible
           </Typography>
         </div>
-        <div className={styles.textboxBody}>
-          <div className={styles.name}>
-            <div className={styles.textArea}>
-              <TextBox
-                name={'FirstName'}
-                label={'First Name (required)'}
-                type={'text'}
-                value={firstName}
-                required={true}
-                placeholder={'Enter your first name'}
-                errorMessage={firstNameError}
-                onChange={(e) => setFirstName(e.target.value.trim())}
-                onBlur={() =>
-                  !NAME_REGEX.test(firstName)
-                    ? setFirstNameError('First Name is invalid')
-                    : setFirstNameError(undefined)
-                }
-              />
-            </div>
-            <div className={styles.textArea}>
-              <TextBox
-                name={'LastName'}
-                label={'Last Name (required)'}
-                type={'text'}
-                value={lastName}
-                required={true}
-                placeholder={'Enter your last name'}
-                errorMessage={lastNameError}
-                onBlur={() =>
-                  !NAME_REGEX.test(lastName)
-                    ? setLastNameError('Last Name is invalid')
-                    : setLastNameError(undefined)
-                }
-                onChange={(e) => setLastName(e.target.value.trim())}
-              />
-            </div>
-          </div>
-          <div className={styles.textArea}>
-            <TextBox
-              name={'PhoneNumber'}
-              label={'Phone Number (required)'}
-              type={'text'}
-              value={phoneNumber}
-              errorMessage={phoneNumberError}
-              required={false}
-              placeholder={'Enter your phone number'}
-              onBlur={() => {
-                !PHONE_REGEX.test(phoneNumber)
-                  ? setPhoneNumberError('Phone number is invalid')
-                  : setPhoneNumberError(undefined);
-              }}
-              onChange={(e) => setPhoneNumber(e.target.value.trim())}
-            />
-          </div>
-          <div className={styles.textArea}>
-            <TextBox
-              variant={TextboxVariant.LONGER}
-              name={'Email'}
-              label={'Email Address (required)'}
-              type={'email'}
-              value={email}
-              required={true}
-              errorMessage={emailError}
-              placeholder={'Enter your email address'}
-              onBlur={() =>
-                !EMAIL_REGEX.test(email)
-                  ? setEmailError('Email Address is invalid')
-                  : setEmailError(undefined)
-              }
-              onChange={(e) => setEmail(e.target.value.trim())}
-            />
-          </div>
-          <div className={styles.textArea}>
-            <TextBox
-              variant={TextboxVariant.LONGER}
-              name={'jobtitle'}
-              label={'Job Title (required)'}
-              type={'text'}
-              value={jobtitle}
-              required={true}
-              placeholder={'Teacher, Student, Institute, Other'}
-              errorMessage={jobtitleError}
-              onBlur={() =>
-                !jobtitle.trim()
-                  ? setJobtitleError('The Job title is invalid')
-                  : setJobtitleError(undefined)
-              }
-              onChange={(e) => setJobtitle(e.target.value.trim())}
-            />
-          </div>
-          <div className={`${styles.textArea} ${styles.message}`}>
-            <TextArea
-              name={'Message'}
-              label={'Message'}
-              value={message}
-              required={false}
-              placeholder={'Enter your message'}
-              maxlength={300}
-              onBlur={() =>
-                !message.trim()
-                  ? setMessageError('')
-                  : setMessageError(undefined)
-              }
-              onChange={(e) => setMessage(e.target.value)}
-            ></TextArea>
-          </div>
-          <ActionButton
-            type='submit'
-            label='Submit'
-            style={BUTTON_STYLE.Primary}
-            className={styles.primaryBtn}
-          />
-          {submissionSuccess && (
-            <div className={styles.success}>
-              <Typography variant={TypographyVariant.Body1}>
-                Thank you for contacting us. We will get back to you shortly.
-              </Typography>
-            </div>
-          )}
-        </div>
       </form>
+      <div className={styles.textboxBody}>
+        <div className={styles.name}>
+          <div className={styles.textArea}>
+            <TextBox
+              name={'FirstName'}
+              label={'First Name (required)'}
+              type={'text'}
+              value={firstName}
+              required={true}
+              placeholder={'Enter your first name'}
+              errorMessage={firstNameError}
+              onChange={(e) => setFirstName(e.target.value.trim())}
+              onBlur={() =>
+                !NAME_REGEX.test(firstName)
+                  ? setFirstNameError('First Name is invalid')
+                  : setFirstNameError(undefined)
+              }
+            />
+          </div>
+          <div className={styles.textArea}>
+            <TextBox
+              name={'LastName'}
+              label={'Last Name (required)'}
+              type={'text'}
+              value={lastName}
+              required={true}
+              placeholder={'Enter your last name'}
+              errorMessage={lastNameError}
+              onBlur={() =>
+                !NAME_REGEX.test(lastName)
+                  ? setLastNameError('Last Name is invalid')
+                  : setLastNameError(undefined)
+              }
+              onChange={(e) => setLastName(e.target.value.trim())}
+            />
+          </div>
+        </div>
+        <div className={styles.textArea}>
+          <TextBox
+            name={'PhoneNumber'}
+            label={'Phone Number (required)'}
+            type={'text'}
+            value={phoneNumber}
+            errorMessage={phoneNumberError}
+            required={false}
+            placeholder={'Enter your phone number'}
+            onBlur={() => {
+              !PHONE_REGEX.test(phoneNumber)
+                ? setPhoneNumberError('Phone number is invalid')
+                : setPhoneNumberError(undefined);
+            }}
+            onChange={(e) => setPhoneNumber(e.target.value.trim())}
+          />
+        </div>
+        <div className={styles.textArea}>
+          <TextBox
+            variant={TextboxVariant.LONGER}
+            name={'Email'}
+            label={'Email Address (required)'}
+            type={'email'}
+            value={email}
+            required={true}
+            errorMessage={emailError}
+            placeholder={'Enter your email address'}
+            onBlur={() =>
+              !EMAIL_REGEX.test(email)
+                ? setEmailError('Email Address is invalid')
+                : setEmailError(undefined)
+            }
+            onChange={(e) => setEmail(e.target.value.trim())}
+          />
+        </div>
+        <div className={styles.textArea}>
+          <TextBox
+            variant={TextboxVariant.LONGER}
+            name={'jobtitle'}
+            label={'Job Title (required)'}
+            type={'text'}
+            value={jobtitle}
+            required={true}
+            placeholder={'Teacher, Student, Institute, Other'}
+            errorMessage={jobtitleError}
+            onBlur={() =>
+              !jobtitle.trim()
+                ? setJobtitleError('The Job title is invalid')
+                : setJobtitleError(undefined)
+            }
+            onChange={(e) => setJobtitle(e.target.value.trim())}
+          />
+        </div>
+        <div className={`${styles.textArea} ${styles.message}`}>
+          <TextArea
+            name={'Message'}
+            label={'Message'}
+            value={message}
+            required={false}
+            placeholder={'Enter your message'}
+            maxlength={300}
+            onBlur={() =>
+              !message.trim() ? setMessageError('') : setMessageError(undefined)
+            }
+            onChange={(e) => setMessage(e.target.value)}
+          ></TextArea>
+        </div>
+        <ActionButton
+          type='submit'
+          label='Submit'
+          style={BUTTON_STYLE.Primary}
+          className={styles.primaryBtn}
+        />
+        {submissionSuccess && (
+          <div className={styles.success}>
+            <Typography variant={TypographyVariant.Body1}>
+              Thank you for contacting us. We will get back to you shortly.
+            </Typography>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
