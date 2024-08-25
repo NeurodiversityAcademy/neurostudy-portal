efa279aeba0be5c2b76e3e6441229e889bd347f8
Salman Hossain <salmanhossain_007@yahoo.com>
2024-08-25 20:39:41 +1000

    Populating profile header info

--- src/app/components/profile/ProfileBodyHeader.tsx
+++ src/app/components/profile/ProfileBodyHeader.tsx
@@ -1,7 +1,28 @@
+import Typography, { TypographyVariant } from '../typography/Typography';
 import styles from './profileBodyHeader.module.css';
 
+const user: Record<string, string | number> = {
+  username: 'Olivia Rhyme',
+  email: 'Olivia@gmail.com',
+  age: 24,
+};
+
 const ProfileBodyHeader: React.FC = () => {
-  return <div className={styles.container}>Olivia Rhyme</div>;
+  const { username, email, age } = user;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}></div>
+      <div className={styles.content}>
+        <Typography variant={TypographyVariant.H3} className='mb-0'>
+          {username}
+        </Typography>
+        <Typography variant={TypographyVariant.Body3} color='var(--grey-300)'>
+          {[email, age ? age + ' Years' : ''].filter((v) => v).join(' | ')}
+        </Typography>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileBodyHeader;
