10c8d60c98d1ab882a60f074cd3c7c0516a897fe
Salman Hossain <salmanhossain_007@yahoo.com>
2024-09-03 19:46:40 +1000

    Profile - integrating readonly mode with preference section

--- src/app/components/profile/ProfilePreferenceSection/index.tsx
+++ src/app/components/profile/ProfilePreferenceSection/index.tsx
@@ -1,36 +1,21 @@
 'use client';
 
-import { useForm, UseFormReturn } from 'react-hook-form';
-import Form from '../../formElements/Form';
-import TextBox from '../../formElements/TextBox/TextBox';
 import preferenceIcon from '@/app/images/preferenceIcon.svg';
 import ProfileCard from '../ProfileCard';
-import TextArea from '../../formElements/TextArea/TextArea';
 import { useProfileContext } from '@/app/utilities/profile/ProfileProvider';
-import {
-  forwardRef,
-  ForwardRefExoticComponent,
-  RefAttributes,
-  useImperativeHandle,
-} from 'react';
 import { ProfileSectionRef } from '@/app/interfaces/Profile';
+import ProfilePreferenceForm from './Form';
+import { forwardRef, ForwardRefExoticComponent, RefAttributes } from 'react';
+import ProfileAttributes from '../ProfileAttributes';
+import getProfileSectionData from '@/app/utilities/profile/getProfileSectionData';
+import { PREFERENCE_FIELDS } from '@/app/utilities/profile/constants';
 
 const ProfilePreferenceSection: ForwardRefExoticComponent<
   RefAttributes<ProfileSectionRef>
-> = forwardRef<ProfileSectionRef>((_, ref) => {
-  const { data, isLoading } = useProfileContext();
+> = forwardRef<ProfileSectionRef>((_, formRef) => {
+  const { data: _data, isLoading, isEditing } = useProfileContext();
 
-  const methods: UseFormReturn = useForm({
-    mode: 'onBlur',
-  });
-
-  useImperativeHandle(
-    ref,
-    () => ({
-      methods,
-    }),
-    [methods]
-  );
+  const data = _data && getProfileSectionData(_data, PREFERENCE_FIELDS);
 
   return (
     <ProfileCard
@@ -40,31 +25,11 @@ const ProfilePreferenceSection: ForwardRefExoticComponent<
       collapsible
       isLoading={isLoading}
     >
-      <Form initialized={!isLoading} methods={methods}>
-        <TextBox
-          name='Conditions'
-          label='Tell us about your Neuro-Condition'
-          showLabel
-          placeholder='E.G. ADHD'
-          helperText='This will help us create personalised experience for you'
-          defaultValue={data?.Conditions?.join(', ') || ''}
-        />
-        <TextBox
-          name='Institutions'
-          label='Learning Institutions'
-          showLabel
-          placeholder='E.G. California University'
-          helperText='This will help us create personalised experience for you'
-          defaultValue={data?.Institutions?.join(', ') || ''}
-        />
-        <TextArea
-          name='EnvDescription'
-          label='Describe a learning environment that you find ideal*'
-          showLabel
-          placeholder='Ex. - I prefer a remote setup with an option to opt for hybrid system'
-          defaultValue={data?.EnvDescription || ''}
-        />
-      </Form>
+      {isEditing ? (
+        <ProfilePreferenceForm ref={formRef} />
+      ) : (
+        data && <ProfileAttributes data={data} />
+      )}
     </ProfileCard>
   );
 });
