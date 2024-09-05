2253b35dc366df6e3db20ea568c0576f5e85cdc6
Salman Hossain <salmanhossain_007@yahoo.com>
2024-09-05 21:45:35 +1000

    Profile - setting types for Section & SectionForms

--- src/app/interfaces/Profile.ts
+++ src/app/interfaces/Profile.ts
@@ -1,6 +1,33 @@
-import { UseFormReturn } from 'react-hook-form';
+import { SubmitHandler, UseFormReturn } from 'react-hook-form';
 import { UserProps } from './User';
+import { MouseEventHandler } from 'react';
 
 export interface ProfileSectionRef {
   methods: UseFormReturn<UserProps>;
+}
+
+type BaseProfileSectionProps = {
+  onSectionEdit?: MouseEventHandler<HTMLButtonElement>;
+  popup?: boolean;
+};
+
+type ProfileSectionPopupTrueProps = BaseProfileSectionProps & {
+  popup: true;
+  onSubmit: SubmitHandler<UserProps>;
+  onCancel?: MouseEventHandler<HTMLButtonElement>;
+};
+
+type ProfileSectionPopupFalseProps = BaseProfileSectionProps & {
+  popup?: false;
+  onSubmit?: never;
+  onCancel?: never;
+};
+
+export type ProfileSectionProps =
+  | ProfileSectionPopupTrueProps
+  | ProfileSectionPopupFalseProps;
+
+export interface ProfileSectionFormProps {
+  onSubmit?: SubmitHandler<UserProps>;
+  onCancel?: MouseEventHandler<HTMLButtonElement>;
 }
