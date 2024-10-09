9a4268e2238cdec75655551dfc65bbebb510a8ca
Salman Hossain <salmanhossain_007@yahoo.com>
2024-10-09 16:52:16 +1100

    Course: no result content

--- src/app/components/course/CourseSearch/CourseSearchResult.tsx
+++ src/app/components/course/CourseSearch/CourseSearchResult.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import Image from 'next/image';
 import Loader from '../../loader/Loader';
 import CourseCard from '../CourseCard';
 import styles from './courseSearchResult.module.css';
 import { useCourseContext } from '@/app/utilities/course/CourseProvider';
+import noCoursesSrc from '@/app/images/no_courses.jpg';
 
 const CourseSearchResult: React.FC = () => {
   const { data, isLoading } = useCourseContext();
@@ -13,6 +15,14 @@ const CourseSearchResult: React.FC = () => {
       {data?.map((course) => (
         <CourseCard key={course.InstitutionName} course={course} />
       ))}
+      {!isLoading && !data?.length && (
+        <div className={styles.noResult}>
+          <Image src={noCoursesSrc} alt='No courses found.' />
+          <div role='alert' className={styles.noResultText} aria-atomic='true'>
+            Sorry, there are no results for the applied filter(s).
+          </div>
+        </div>
+      )}
       <Loader isLoading={isLoading} alignTop />
     </section>
   );
