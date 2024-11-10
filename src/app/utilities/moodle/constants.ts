6b1c9ab759e58ae8ebf50debc0a54c2edde4e6ce
Salman Hossain <salmanhossain_007@yahoo.com>
2024-11-11 01:58:20 +1100

    Stripe, Moodle - removing unused env variables

--- src/app/utilities/moodle/constants.ts
+++ src/app/utilities/moodle/constants.ts
@@ -1,22 +1,14 @@
 export const MOODLE_STUDENT_ROLE_ID = 5;
 
-const MOODLE_INTRO_COURSE_ID: number = +(
-  process.env.MOODLE_INTRO_COURSE_ID ?? '0'
-);
 const MOODLE_SECRET: string = process.env.MOODLE_SECRET ?? '';
 const MOODLE_HOST_URL: string = process.env.MOODLE_HOST_URL ?? '';
-const DEV_MOODLE_INTRO_COURSE_ID: number = +(
-  process.env.DEV_MOODLE_INTRO_COURSE_ID ?? '0'
-);
 const DEV_MOODLE_SECRET: string = process.env.DEV_MOODLE_SECRET ?? '';
 const DEV_MOODLE_HOST_URL: string = process.env.DEV_MOODLE_HOST_URL ?? '';
 
 if (
   process.env.NODE_ENV !== 'development' &&
-  (!MOODLE_INTRO_COURSE_ID ||
-    !MOODLE_SECRET ||
+  (!MOODLE_SECRET ||
     !MOODLE_HOST_URL ||
-    !DEV_MOODLE_INTRO_COURSE_ID ||
     !DEV_MOODLE_SECRET ||
     !DEV_MOODLE_HOST_URL)
 ) {
@@ -24,10 +16,8 @@ if (
 }
 
 export {
-  MOODLE_INTRO_COURSE_ID,
   MOODLE_SECRET,
   MOODLE_HOST_URL,
-  DEV_MOODLE_INTRO_COURSE_ID,
   DEV_MOODLE_SECRET,
   DEV_MOODLE_HOST_URL,
 };
