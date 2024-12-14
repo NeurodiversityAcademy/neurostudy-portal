3c76bdc5d22ffa55e11fbcf691d79be7fa197b3d
Salman Hossain <salmanhossain_007@yahoo.com>
2024-12-14 19:12:37 +1100

    SignUp - listing the Year in desc order

--- src/app/utilities/auth/constants.ts
+++ src/app/utilities/auth/constants.ts
@@ -70,7 +70,9 @@ export const OPTIONS_MONTH = [
   { label: 'Dec', value: 11 },
 ];
 
+const curYear = new Date().getFullYear();
+
 export const OPTIONS_YEAR = Array.from({ length: 100 }, (_, i) => ({
-  label: (1915 + i).toString(),
-  value: 1915 + i,
+  label: (curYear - 10 - i).toString(),
+  value: curYear - 10 - i,
 }));
