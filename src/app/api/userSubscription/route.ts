4e27acb9e618a6638867cd96967e0a833cd78fb3
Salman Hossain <salmanhossain_007@yahoo.com>
2024-12-25 20:52:03 +1100

    Pulling handbook content in the backend and then passing onto client

--- src/app/api/userSubscription/route.ts
+++ src/app/api/userSubscription/route.ts
@@ -6,27 +6,51 @@ import {
 import { registerCRMContact } from '@/app/utilities/register/registerCRMContact';
 import { isValidUserSubscriptionData } from '@/app/utilities/validation/validateUserSubscriptionData';
 import CRMCreateResponseInterface from '@/app/interfaces/CRMCreateResponseInterface';
+import { consumeRateWithIp } from '@/app/utilities/api/rateLimiter';
+import processAPIError from '@/app/utilities/api/processAPIError';
+import { NextRequest } from 'next/server';
 
-export async function POST(request: Request) {
-  const data: UserSubscriptionHandbookType = await request.json();
-  const subscriptionData: UserSubscriptionType = (() => {
-    const newData = { ...data };
-    delete newData.getHandbook;
-    return newData;
-  })();
+export async function POST(request: NextRequest) {
+  try {
+    await consumeRateWithIp(request);
 
-  if (isValidUserSubscriptionData(subscriptionData)) {
-    const response: CRMCreateResponseInterface | boolean =
-      await registerCRMContact(subscriptionData);
+    const data: UserSubscriptionHandbookType = await request.json();
+    const subscriptionData: UserSubscriptionType = (() => {
+      const newData = { ...data };
+      delete newData.getHandbook;
+      return newData;
+    })();
 
-    if (data.getHandbook === true) {
-      // NOTE
-      // We send the document regardless of failure
-      return new Response(JSON.stringify('Handbook processing in progress.'));
+    if (isValidUserSubscriptionData(subscriptionData)) {
+      const response: CRMCreateResponseInterface | boolean =
+        await registerCRMContact(subscriptionData);
+
+      if (data.getHandbook === true) {
+        // NOTE
+        // We send the document regardless of failure
+
+        const response = await fetch(process.env.HANDBOOK_SOURCE_URL || 'N/A');
+
+        if (!response.ok) {
+          return new Response('Failed to fetch PDF', { status: 500 });
+        }
+
+        const pdfBuffer = await response.arrayBuffer();
+
+        return new Response(pdfBuffer, {
+          status: 200,
+          headers: {
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment; filename="NDA Handbook.pdf"',
+          },
+        });
+      }
+
+      return new Response(JSON.stringify(response));
     }
 
-    return new Response(JSON.stringify(response));
+    return returnBadResponse();
+  } catch (ex) {
+    return processAPIError(ex instanceof Error ? ex : null);
   }
-
-  return returnBadResponse();
 }
