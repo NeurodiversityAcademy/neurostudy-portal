48b41f129346d0a8d2cc03f7ba28885cf2a927c9
Pratik Bhumkar <150103212+bhumkarpratik@users.noreply.github.com>
2025-01-19 12:14:31 +1100

    Code review comments

--- src/app/components/article/MetaPixel.tsx
+++ src/app/components/article/MetaPixel.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
 import React, { useEffect } from 'react';
 
+const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID ?? '';
 export default function MetaPixel(): JSX.Element {
-  const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID ?? '';
+  const pathname = usePathname();
   useEffect(() => {
     import('react-facebook-pixel')
       .then((x) => x.default)
@@ -11,7 +13,7 @@ export default function MetaPixel(): JSX.Element {
         ReactPixel.init(FB_PIXEL_ID);
         ReactPixel.pageView();
       });
-  });
+  }, [pathname]);
 
   return <></>;
 }
