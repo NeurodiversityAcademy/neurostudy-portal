e16101a16cda5c5eac93ea938eaba80fe0b98d05
Pratik Bhumkar <150103212+bhumkarpratik@users.noreply.github.com>
2025-07-11 22:34:58 +1000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,10 @@ export default async function RootLayout({
   return (
     <html lang='en' className={poppins.variable}>
       <head>
-        <meta name="google-site-verification" content="djfPTzD9D2f3d1fmQBIqJqV_H7SQFbPHimsnYKBI66s" />
+        <meta
+          name='google-site-verification'
+          content='djfPTzD9D2f3d1fmQBIqJqV_H7SQFbPHimsnYKBI66s'
+        />
         <link rel='icon' href='/favicon.ico' sizes='any' />
       </head>
       <body className={poppins.className}>
