201d44ae8e7c1094c8ffc631e719467c2711c53d
Pratik Bhumkar <150103212+bhumkarpratik@users.noreply.github.com>
2025-07-11 23:12:52 +1000

    enable index and follow

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -3,8 +3,8 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Neurodiversity Components',
   robots: {
-    index: false,
-    follow: false,
+    index: true,
+    follow: true,
   },
 };
 
